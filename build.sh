1b09a78d6d49758d1efa47d75fef141ed10ec4e1
David Sherret <dsherret@gmail.com>
2020-08-03 16:55:31 -0400

    feat: Upgrade dprint-core.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CFG_RELEASE=1.45.0-nightly
 export CFG_RELEASE_CHANNEL=nightly
-cargo +nightly build --release --target wasm32-unknown-unknown --verbose
+cargo +nightly build --release --features "wasm" --target wasm32-unknown-unknown --verbose
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,9 @@ mod format;
 
 pub use format::*;
 
+#[cfg(feature = "wasm")]
 #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
 mod wasm_plugin;
-
+#[cfg(feature = "wasm")]
 #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
 pub use wasm_plugin::*;
