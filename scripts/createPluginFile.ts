6f822c443fa7cae9c444ca8ecbaaa9826a923528
David Sherret <dsherret@gmail.com>
2022-01-30 16:28:54 -0500

    chore(release): 0.5.1
    Try again.

--- scripts/createPluginFile.ts
+++ scripts/createPluginFile.ts
@@ -1,6 +1,3 @@
-import { createHash } from "https://deno.land/std@0.122.0/hash/mod.ts";
-import * as hex from "https://deno.land/std@0.122.0/encoding/hex.ts";
-
 const packageText = Deno.readTextFileSync("Cargo.toml");
 // version = "x.x.x"
 const version = packageText.match(/version\s*=\s*\"(\d+\.\d+\.\d+)\"/)![1];
@@ -13,20 +10,26 @@ const outputFile = {
     schemaVersion: 1,
     name: "dprint-plugin-rustfmt",
     version,
-    "mac-x86_64": getPlatformObject("dprint-plugin-rustfmt-x86_64-apple-darwin.zip"),
-    "linux-x86_64": getPlatformObject("dprint-plugin-rustfmt-x86_64-unknown-linux-gnu.zip"),
-    "windows-x86_64": getPlatformObject("dprint-plugin-rustfmt-x86_64-pc-windows-msvc.zip"),
+    "mac-x86_64": await getPlatformObject("dprint-plugin-rustfmt-x86_64-apple-darwin.zip"),
+    "linux-x86_64": await getPlatformObject("dprint-plugin-rustfmt-x86_64-unknown-linux-gnu.zip"),
+    "windows-x86_64": await getPlatformObject("dprint-plugin-rustfmt-x86_64-pc-windows-msvc.zip"),
 };
 Deno.writeTextFile("plugin.exe-plugin", JSON.stringify(outputFile, undefined, 2) + "\n");
 
-function getPlatformObject(zipFileName: string) {
+async function getPlatformObject(zipFileName: string) {
     const fileBytes = Deno.readFileSync(zipFileName);
-    const hash = createHash("sha256");
-    hash.update(fileBytes);
-    const checksum = hex.encodeToString(new Uint8Array(hash.digest()));
+    const checksum = await getChecksum(fileBytes);
     console.log(zipFileName + ": " + checksum);
     return {
         "reference": `https://github.com/dprint/dprint-plugin-rustfmt/releases/download/${version}/${zipFileName}`,
         "checksum": checksum,
     };
+}
+
+async function getChecksum(bytes: Uint8Array) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+    const hashBuffer = await crypto.subtle.digest("SHA-256", bytes);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
 }
