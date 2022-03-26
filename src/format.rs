6fb6a8c550a807c39c8bd19c23fd8d031cfaa6ad
David Sherret <dsherret@users.noreply.github.com>
2022-03-26 19:40:56 -0400

    feat: upgrade to process plugin schema v4 (#9)

--- src/format.rs
+++ src/format.rs
@@ -1,13 +1,20 @@
+use crate::configuration::resolve_config;
+
 use super::configuration::Configuration;
 
-use anyhow::{bail, Result};
-use rustfmt_nightly::{Input, Session};
+use anyhow::bail;
+use anyhow::Result;
+use rustfmt_nightly::Input;
+use rustfmt_nightly::Session;
 
-pub fn format_text(file_text: &str, config: &Configuration) -> Result<String> {
+pub fn format_text(file_text: String, config: &Configuration) -> Result<Option<String>> {
+  let original_text = file_text.clone();
   let mut out = Vec::new();
   {
-    let input = Input::Text(String::from(file_text));
-    let mut session = Session::new(config.rustfmt_config.clone(), Some(&mut out));
+    let input = Input::Text(file_text);
+    // config is not `Sync`, so unfortunately we have to recreate it each time
+    let (config, _) = resolve_config(&config.config, &config.global_config);
+    let mut session = Session::new(config, Some(&mut out));
     session.format(input)?;
   }
 
@@ -16,28 +23,30 @@ pub fn format_text(file_text: &str, config: &Configuration) -> Result<String> {
   let text = text
     .trim_start_matches("<stdin>:\n\n")
     .trim_start_matches("stdin:\n\n");
-  if text.trim().is_empty() && file_text.trim().len() > 0 {
+  if text.trim().is_empty() && original_text.trim().len() > 0 {
     bail!("Rustfmt had errors.")
   }
-  Ok(text.to_string())
+  if original_text == text {
+    Ok(None)
+  } else {
+    Ok(Some(text.to_string()))
+  }
 }
 
 #[cfg(test)]
 mod test {
-  use dprint_core::configuration::resolve_global_config;
-
-  use crate::configuration::resolve_config;
-
   use super::*;
 
   #[test]
   fn test_format() {
-    let global_config = resolve_global_config(Default::default(), &Default::default()).config;
-    let config = resolve_config(Default::default(), &global_config).config;
-    assert_eq!(format_text("use test;", &config).unwrap(), "use test;\n");
-    assert_eq!(format_text("use test;\n", &config).unwrap(), "use test;\n");
+    let config = Configuration {
+        config: Default::default(),
+        global_config: Default::default(),
+    };
+    assert_eq!(format_text("use test;".to_string(), &config).unwrap(), Some("use test;\n".to_string()));
+    assert_eq!(format_text("use test;\n".to_string(), &config).unwrap(), None);
     assert_eq!(
-      format_text("let test = ...;", &config)
+      format_text("let test = ...;".to_string(), &config)
         .err()
         .unwrap()
         .to_string(),
